97d51da47aeb9debf275d0f962197a5636ce4282
Adrielly.CS <165967593+Adriellyc@users.noreply.github.com>
2025-06-07 04:11:43 +0000

    A

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Conselho from "./components/Conselho";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-black text-gray-300">
